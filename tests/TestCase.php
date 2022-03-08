4106ca3e6592bb00b78986d2bb607116b53ed8d3
Antonio <antonio@agrocity.eu>
2022-03-08 17:18:03 +0200

    Created branch for Laravel 9

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,14 +9,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
 {
 	use RefreshDatabase;
 	
-	//protected function setUp(): void
-	//{
-	//	parent::setUp();
-	//
-	//	//migrate the test users table
-	//	TestUsersTable::migrate();
-	//}
-	
 	protected function getPackageProviders($app)
 	{
 		return [
