c6c02807431206d9000488bb7c6453ba73c2c37e
Antonio <antonio@agrocity.eu>
2022-09-14 23:47:13 +0300

    Added the option to generate a basic / complex bapi file

--- src/Console/Commands/MakeBapiCommand.php
+++ src/Console/Commands/MakeBapiCommand.php
@@ -5,14 +5,29 @@ use AntonioPrimera\Artisan\FileGeneratorCommand;
 
 class MakeBapiCommand extends FileGeneratorCommand
 {
-	protected $signature = "make:bapi {name}";
+	protected $signature = "make:bapi
+		{name : The name of the bapi file, including the relative folder structure}
+		{--full : Whether to generate a complex bapi class, with all available methods and hooks (by default it generates a simple bapi class)}";
+	
 	protected $description = "Create a new Bapi class file in: app/Bapis/";
 	
 	protected function recipe(): array
 	{
+		$stubFiles = [
+			'simple' => __DIR__ . '/stubs/BapiStubBasic.php.stub',
+			'complex' => __DIR__ . '/stubs/BapiStubComplex.php.stub'
+		];
+		
+		$shouldGenerateComplexBapi = $this->hasOption('full')
+			|| env('BAPI_GENERATOR_COMPLEX_BAPIS', false);
+		
+		$stubFile = $shouldGenerateComplexBapi
+			? $stubFiles['complex']
+			: $stubFiles['simple'];
+		
 		return [
 			'Bapi File' => [
-				'stub' => __DIR__ . '/stubs/BapiStub.php.stub',
+				'stub' => $stubFile,
 				'path' => 'Bapis',
 				'rootNamespace' => 'App\\Bapis',
 				'replace' => [
